22fec35e2fe94b20de2548ccf28f74d4d9803ada
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:41:15 +0500

    Created .next/static/chunks/app_layout_c0237562.js

---
+++ .next/static/chunks/app_layout_c0237562.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a917f39f._.css",
+  "static/chunks/node_modules_9adc9b6e._.js",
+  "static/chunks/components_f818f0c7._.js"
+],
+    source: "dynamic"
+});
