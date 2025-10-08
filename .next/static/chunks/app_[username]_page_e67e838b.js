90cc51e1afcef2dd2b475e098aa994bd3dea8dd6
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:41:52 +0500

    Created .next/static/chunks/app_[username]_page_e67e838b.js

---
+++ .next/static/chunks/app_[username]_page_e67e838b.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_[username]_page_97901755.js"
+],
+    source: "dynamic"
+});
