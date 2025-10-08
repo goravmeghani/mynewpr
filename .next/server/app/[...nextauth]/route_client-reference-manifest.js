e81010af0ee04840c188e7c404a435f1d17414f5
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:25:53 +0500

    Created .next/server/app/[...nextauth]/route_client-reference-manifest.js

---
+++ .next/server/app/[...nextauth]/route_client-reference-manifest.js
@@ -0,0 +1,2 @@
+globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
+globalThis.__RSC_MANIFEST["/[...nextauth]/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
