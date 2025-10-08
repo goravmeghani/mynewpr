44d020f5533870b1840ee46ad116111bac89d79a
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:41:10 +0500

    Created .next/static/chunks/app_layout_b8ac3782.js

---
+++ .next/static/chunks/app_layout_b8ac3782.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a917f39f._.css",
+  "static/chunks/node_modules__pnpm_bb5e1580._.js",
+  "static/chunks/components_f818f0c7._.js"
+],
+    source: "dynamic"
+});
