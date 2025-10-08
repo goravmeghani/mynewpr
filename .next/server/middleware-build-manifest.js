5e850fb776fa5255abc0323ccf6acd68051d1bb4
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:16:46 +0500

    Uploaded local folder

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,26 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/eec26_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_4237fcca._.js",
+    "static/chunks/eec26_next_dist_compiled_bb5209e6._.js",
+    "static/chunks/eec26_next_dist_client_f51a14fe._.js",
+    "static/chunks/eec26_next_dist_aabb7939._.js",
+    "static/chunks/61dca_@swc_helpers_cjs_e27e0aca._.js",
+    "static/chunks/_e69f0d32._.js",
+    "static/chunks/_f85126fd._.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
