e205b38d77da067fafa7d8b1b438912e7383d58a
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:41:30 +0500

    Created .next/static/chunks/app_page_389b6717.js

---
+++ .next/static/chunks/app_page_389b6717.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/eec26_next_dist_f1920fe6._.js",
+  "static/chunks/32c6d_react-icons_fa_index_mjs_f485fdcd._.js",
+  "static/chunks/32c6d_react-icons_lib_4cdaff55._.js",
+  "static/chunks/components_CenterContent_7198f973.js"
+],
+    source: "dynamic"
+});
