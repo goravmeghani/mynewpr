15f871fd3dbc048430f659c126ce37191f59537e
goravmeghani <161228160+goravmeghani@users.noreply.github.com>
2025-10-08 21:51:39 +0500

    Created app/page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CenterContent from "../components/CenterContent";
+
+export default function Home() {
+  return (
+    <div>
+      <CenterContent />
+    </div>
+  );
+}
